67e87d41b0e4f0c56b57c3ca09985b63d5be4374
Sean MacIsaac <seanjmacisaac@gmail.com>
2015-12-09 09:53:48 -0500

    Removed the need for the sourceMap.

--- Gruntfile.js
+++ Gruntfile.js
@@ -38,7 +38,7 @@ module.exports = function(grunt) {
     uglify : {
       options : {
         banner    : '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
-        sourceMap : true,
+        sourceMap : false,
         mangle    : false
       },
       transition : {
