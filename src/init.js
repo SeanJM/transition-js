4a8de6dfac35960d8e9b75e1291372ab7193bd6d
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-06-22 08:19:21 -0400

    Filters

---
+++ src/init.js
@@ -0,0 +1,12 @@
+var FILTER_DELTA = {
+  back : deltaBack,
+  elastic : deltaElastic,
+  linear : deltaLinear,
+  quadratic : deltaQuadratic,
+};
+
+var FILTER_EASE = {
+  'in' : easeIn,
+  'in-out' : easeInOut,
+  'out' : easeOut,
+};
