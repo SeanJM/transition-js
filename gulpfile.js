40adfbc8bcdad03dc4c88e3c78dec840b541cd00
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-06-28 08:14:20 -0400

    Added uglified version

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,37 @@
 var gulp = require('gulp');
+var wrap = require('gulp-wrap');
 var header = require('gulp-header');
 var footer = require('gulp-footer');
 var concat = require('gulp-concat');
 var cached = require('gulp-cached');
 var sourcemaps = require('gulp-sourcemaps');
 var remember = require('gulp-remember');
+var uglify = require('gulp-uglify');
 
 var scriptsGlob = [
   'src/init.js',
   'src/**/*.js'
 ];
 
-gulp.task('default', function() {
+gulp.task('concat', function() {
   return gulp.src(scriptsGlob)
     .pipe(sourcemaps.init())
+      .pipe(cached('scripts'))        // only pass through changed files
+      .pipe(remember('scripts'))      // add back all files to the stream
+      .pipe(concat('transition.js'))  // do things that require all files
+      .pipe(header('(function () {\n'))
+      .pipe(footer('})();'))
+    .pipe(sourcemaps.write('../public'))
+    .pipe(gulp.dest('public/'));
+});
+
+gulp.task('uglify', function () {
+  return gulp.src(scriptsGlob)
         .pipe(cached('scripts'))        // only pass through changed files
         .pipe(remember('scripts'))      // add back all files to the stream
-        .pipe(concat('transition.js'))  // do things that require all files
-        .pipe(header('(function () {\n'))
-        .pipe(footer('})();'))
-      .pipe(sourcemaps.write('../public'))
+        .pipe(concat('transition.min.js'))  // do things that require all files
+        .pipe(wrap('(function(){<%= contents %>}());'))
+        .pipe(uglify())  // do things that require all files
       .pipe(gulp.dest('public/'));
 });
 
@@ -32,3 +44,5 @@ gulp.task('watch', function () {
     }
   });
 });
+
+gulp.task('default', [ 'concat', 'uglify', 'watch' ]);
