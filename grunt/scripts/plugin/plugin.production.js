0373bf948d1bcf92a3ff600d0c074a43c29b8fc4
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-08-19 16:46:54 -0400

    Added bundle name to package.json

--- grunt/scripts/plugin/plugin.production.js
+++ grunt/scripts/plugin/plugin.production.js
@@ -1,4 +1,7 @@
+const fs = require('fs');
+const path = require('path');
 const files = require('./plugin.files');
+const config = JSON.parse(fs.readFileSync('package.json', 'utf8'));
 
 module.exports = {
   files : files.list,
@@ -9,7 +12,9 @@ module.exports = {
       },
       files : {
         src : files.list,
-        dest : 'bin/bundle.min.js'
+        dest : config.scripts && config.scripts.bundle
+         ? config.scripts.bundle
+         : 'bin/bundle.min.js'
       }
     }
   }
